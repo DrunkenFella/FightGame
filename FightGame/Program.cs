705089eb0fac412f99991ac10b1076a33fa8c6cf
Drunken Fella <alex.ostlund@elev.ga.ntig.se>
2022-09-29 14:13:11 +0200

    pp

--- FightGame/Program.cs
+++ FightGame/Program.cs
@@ -1,2 +1,30 @@
 ﻿// Slump
 
+Random generator = new Random();
+// behöver bara en generator
+
+int hitRoll = generator.Next(100);
+Console.WriteLine(hitRoll);
+
+hitRoll = generator.Next(100);
+Console.WriteLine(hitRoll);
+
+if (hitRoll > 95)
+{
+    Console.WriteLine("Crit!");
+}
+else if (hitRoll > 40)
+{
+    Console.WriteLine("HIT!");
+}
+else 
+{
+    Console.WriteLine("Miss!");
+}
+
+
+Console.ReadLine();
+
+
+// Loopar
+
