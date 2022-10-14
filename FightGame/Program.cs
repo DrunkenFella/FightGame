332a30ea945de21c8360ffa4366096d7ae361d8d
Drunken Fella <alex.ostlund@elev.ga.ntig.se>
2022-10-14 08:37:17 +0200

    pp

--- FightGame/Program.cs
+++ FightGame/Program.cs
@@ -3,28 +3,45 @@
 Random generator = new Random();
 // behöver bara en generator
 
-int hitRoll = generator.Next(100);
-Console.WriteLine(hitRoll);
+Console.CursorVisible = false;
 
-hitRoll = generator.Next(100);
-Console.WriteLine(hitRoll);
-
-if (hitRoll > 95)
+static void Close()
 {
-    Console.WriteLine("Crit!");
-}
-else if (hitRoll > 40)
-{
-    Console.WriteLine("HIT!");
-}
-else 
-{
-    Console.WriteLine("Miss!");
+    Console.ReadKey();
+    Environment.Exit(0);
 }
 
+Console.WriteLine("You are fighting some guy. You both have 10 hp.");
 
 Console.ReadLine();
 
+int hp = 10;
+int hpOpponent = 10;
 
-// Loopar
+Console.WriteLine("You will start the round. Press [enter] to start");
+Console.ReadLine();
 
+while (hp >= 0 && hpOpponent >= 0)
+{
+    int hitRoll = generator.Next(10);
+    int hitRoll2 = generator.Next(10);
+
+    hpOpponent -= hitRoll;
+    Console.WriteLine($"You hit with {hitRoll} damage");
+
+    hp -= hitRoll2;
+    Console.WriteLine($"You get hit with {hitRoll2} damage");
+}
+
+if (hp < 0)
+{
+    Console.WriteLine("You lost");
+    Console.ReadLine();
+    Close();
+}
+else
+{
+    Console.WriteLine("You win");
+    Console.ReadLine();
+    Close();
+}
